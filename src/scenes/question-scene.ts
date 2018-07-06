7be11c73fde91e6ca0bf05dbf57f693722149500
mafiaf <barisfirat@hotmail.com>
2018-07-06 21:26:36 +0200

    Added new questions + start screen name

--- src/scenes/question-scene.ts
+++ src/scenes/question-scene.ts
@@ -29,8 +29,20 @@ export class QuestionScene {
 
         // store all questions in array
         this.questions = [
-            new Question("What movie has the quote 'this is sparta'", ["Bee Movie", "300", "Infinity wars"], 1, this.onAnswerClick),
+            new Question("What movie quote 'this is sparta'", ["Bee Movie", "300", "Infinity wars"], 1, this.onAnswerClick),
+            new Question("What movie quotes 'They'll never take our freedom!'", ["Batman returns", "Braveheart", "Saw"], 1, this.onAnswerClick),
+            new Question("What movie quotes 'It was Beauty killed the Beast'", ["Mulan", "Romeo & Juliet", "King Kong"], 2, this.onAnswerClick),
+            new Question("What movie quotes 'They call it a Royale with cheese.'", ["Pulp Fiction", "Poltergeist", "Some like it hot"], 0, this.onAnswerClick),
             new Question("What movie quotes 'I'm gonna make him an offer he can't refuse'", ["The Godfather", "Fornite", "Iron man"], 0, this.onAnswerClick),
+            new Question("What movie quotes 'Wax on, wax off'", ["12 years a slave", "Karate Kid", "Kung Fury"], 1, this.onAnswerClick),
+            new Question("What movie quotes 'My precious'", ["Lord of the rings", "Argo", "Frankenstein"], 0, this.onAnswerClick),
+            new Question("What movie quotes 'I mean, funny like I'm a clown? I amuse you?'", ["The help", "Star Wars", "Goodfellas"], 2, this.onAnswerClick),
+            new Question("What movie quotes 'Shaken, not stirred'", ["Wonderful life", "Goldfinger", "Cool hand luke"], 1, this.onAnswerClick),
+            new Question("What movie quotes 'Keep your friends close, but your enemies closer'", ["The Godfather", "The usual suspects", "Network"], 0, this.onAnswerClick),
+            new Question("What movie quotes 'Say hello to my little friend'", ["The Graduate", "Dead poets society", "Scarface"], 2, this.onAnswerClick),
+            new Question("What movie quotes 'To infinity and beyond!'", ["Back to the future", "Toy Story", "Goodfellas"], 1, this.onAnswerClick),
+            new Question("What movie quotes 'You can't handle the truth!'", ["The Terminator", "A few good men", "Iron man"], 1, this.onAnswerClick),
+            new Question("What movie quotes 'Why so serious?'", ["Taxi driver", "The dark knight", "Star wars"], 1, this.onAnswerClick),
         ];
 
         // get first question
--- src/scenes/start-scene.ts
+++ src/scenes/start-scene.ts
@@ -3,9 +3,13 @@ import { gameOptions } from "../util";
 import { State } from "../interface";
 
 export class StartScene {
+    private welcometext: PIXI.Text;
     private startBtn: StartButton;
     private state: State;
     constructor(state: State) {
+        this.welcometext = new PIXI.Text("Quiz it up", {fontSize:40});
+        this.welcometext.x = gameOptions.centerWidth - 100;
+        this.welcometext.y = gameOptions.centerHeight - 150;
         this.state = state;
         // center start button in middle of screen
         this.startBtn = new StartButton(gameOptions.centerWidth - 100, gameOptions.centerHeight, this.onStartClick.bind(this));
@@ -19,5 +23,6 @@ export class StartScene {
 
     draw(app: PIXI.Application) {
         app.stage.addChild(this.startBtn.getForStage())
+        app.stage.addChild(this.welcometext)
     }
 }
